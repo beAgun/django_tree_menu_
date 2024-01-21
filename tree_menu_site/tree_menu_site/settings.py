f48c3763826cb81db5fe785bc0fb66cf3d0d7c81
Anastasia <nbegun_314@mail.ru>
2024-01-21 20:00:26 +0300

    prod set-s

--- tree_menu_site/tree_menu_site/settings.py
+++ tree_menu_site/tree_menu_site/settings.py
@@ -9,23 +9,23 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c+feu#4cr6^yab7p9hb5d-sk_id@n69x2ei#u6_*m30ef_ugyt'
+SECRET_KEY = 'bjj-k*+-5ij785698098ftc765+-+5buth*/9u566m779-=*uhyifd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.18.0.1', 'myworkout.whitebeef.ru', 'localhost']
 
 
 # Application definition
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'tree_menu_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tree_menu',
+        'USER': 'djuser',
+        'PASSWORD': 'akella279',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -121,7 +124,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+CSRF_TRUSTED_ORIGINS = ['https://myworkout.whitebeef.ru']
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEDIA_URL = '/media/'
 
